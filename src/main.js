77a13aef21a32c3af625de5c93210134adfadd68
lourencofelipe <felipe.pucsc@gmail.com>
2020-04-01 17:14:25 -0300

    Adding history mode

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Contatos from './views/contatos/Contatos.vue'
+import Home from './views/Home.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 const router = new VueRouter({
-  routes: []
+  mode: 'history',
+  routes: [
+    //Ao acessar a rota contatos, o componente que será utilizado será o contatos
+    {path: '/contatos', component: Contatos},
+    {path: '/', component: Home}
+  ]
 })
-
 
 new Vue({
   router,
