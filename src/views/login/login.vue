b4929e70e14c3afa86935b8727c82d39af7fab28
lourencofelipe <felipe.pucsc@gmail.com>
2020-04-06 12:05:14 -0300

    Adding login component

---
+++ src/views/login/login.vue
@@ -0,0 +1,74 @@
+<template>
+    <form class="form-sigin" @submit.prevent="entrar">
+        <h1 class="h3 mb-3 font-wight-normal">Faça seu login</h1>
+        <label class="sr-only">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          placeholder="Email"
+          required
+          autofocus
+          v-model="usuario.email"
+        >
+        <label class="sr-only">Senha</label>
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Senha"
+          required
+          v-model="usuario.senha"
+        >
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+        <div class="alert alert-danger mt-3" v-if="mensagem">{{ mensagem }}</div>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: auto;
+    }
+
+    .form-signin .checkbox {
+        font-weight: 400;
+    }
+
+    .form-signin .form-control {
+        position: relative;
+        box-sizing: border-box;
+        height: auto;
+        padding: 10px;
+        font-size: 16px;
+    }
+
+    .form-signin .form-control:focus {
+        z-index: 2;
+    }
+
+    .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+
+    .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+    }
+
+</style>
+
